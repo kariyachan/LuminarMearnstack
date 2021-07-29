2cff535c54d7a94a54971cc4a0797fa683400e03
kariyachan <kariyachenthomas33@gmail.comgit config --global user.name kariyachangit commit -mtestexit..exitgit config --global user.email kariyachenthomas33@gmail.com>
2021-07-29 11:32:54 +0530

    29jul-sort

--- javascriptworks/flowcontrols/lgofthree.js
+++ javascriptworks/flowcontrols/lgofthree.js
@@ -1,3 +1,3 @@
-var num1=20,num2=15,num3=18;
-var res=(num1>num2 | num1>num3)&(num1<num2 | num1<num3)?num1:(num2>num1 | num2>num3)&(num2<num1 | num2<num3)?num2:num3;
+var num1=20,num2=150,num3=18;
+var res=num1>num2 & num1>num3?num1:num2>num1 & num2>num3?num2:num3;
 console.log(res);
--- javascriptworks/flowcontrols/looping/sortnum.js
+++ javascriptworks/flowcontrols/looping/sortnum.js
@@ -1,5 +1,6 @@
-var num1=10,num2=8,num3=9;
-var i=1;
-while(i>3){
- 
-}
+var num1=10,num2=18,num3=9;
+var b=0;
+var a=Math.max(num1,num2,num3);
+var res=(num1>num2 | num1>num3)&(num1<num2 | num1<num3)?b=num1:(num2>num1 | num2>num3)&(num2<num1 | num2<num3)?b=num2:b=num3;
+var c=Math.min(num1,num2,num3)
+console.log("sorted nos: ",c,b,a);
